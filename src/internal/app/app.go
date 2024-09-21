44ebc280200317c3ff5e656bffe4d2a3a82c3d6d
jenisepl <jenisepl@student.21-school.ru>
2024-09-21 13:57:37 +0300

    parse dir for find

--- src/internal/app/app.go
+++ src/internal/app/app.go
@@ -1,7 +1,25 @@
 package app
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func MyFind() {
-	fmt.Println("Test")
+	flagFile := flag.Bool("f", false, "Флаг для отображения файлов")
+	flagDir := flag.Bool("d", false, "Флаг для отображения директорий")
+	flagLink := flag.Bool("sk", false, "Флаг для отображения символических ссылок")
+	flag.Parse()
+	if !*flagFile && !*flagDir && !*flagLink {
+		*flagFile = true
+		*flagDir = true
+		*flagLink = true
+	}
+	if len(os.Args) < 2 {
+		fmt.Println("необходимо указать директорию\n Usage: ~$ ./myFind /foo")
+		os.Exit(1)
+	}
+	directoryPath := os.Args[1]
+	fmt.Println(directoryPath)
 }
