b73ebab379fa25adb724d3138a3e0c4f74af351f
jenisepl <jenisepl@student.21-school.ru>
2024-09-21 13:59:56 +0300

    add func parse flag

--- src/internal/app/app.go
+++ src/internal/app/app.go
@@ -1,21 +1,25 @@
 package app
 
 import (
-	"flag"
 	"fmt"
 	"os"
 )
 
+//func parseFlag() (bool, bool, bool) {
+//	flagFile := flag.Bool("f", false, "Флаг для отображения файлов")
+//	flagDir := flag.Bool("d", false, "Флаг для отображения директорий")
+//	flagLink := flag.Bool("sl", false, "Флаг для отображения символических ссылок")
+//	flag.Parse()
+//	if !*flagFile && !*flagDir && !*flagLink {
+//		*flagFile = true
+//		*flagDir = true
+//		*flagLink = true
+//	}
+//	return *flagFile, *flagDir, *flagLink
+//}
+
 func MyFind() {
-	flagFile := flag.Bool("f", false, "Флаг для отображения файлов")
-	flagDir := flag.Bool("d", false, "Флаг для отображения директорий")
-	flagLink := flag.Bool("sk", false, "Флаг для отображения символических ссылок")
-	flag.Parse()
-	if !*flagFile && !*flagDir && !*flagLink {
-		*flagFile = true
-		*flagDir = true
-		*flagLink = true
-	}
+	//flagF, flagD, flagSL := parseFlag()
 	if len(os.Args) < 2 {
 		fmt.Println("необходимо указать директорию\n Usage: ~$ ./myFind /foo")
 		os.Exit(1)
