f1e54efdc925fd11b7989be70e0247178ab6dd70
jenisepl <jenisepl@student.21-school.ru>
2024-09-21 16:42:22 +0300

    add link

--- src/internal/service/finder.go
+++ src/internal/service/finder.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -12,6 +13,7 @@ func Finder(directoryPath string, flagF, flagD, flagSL bool) {
 		fmt.Println("Не удалось открыть директорию", directoryPath)
 		os.Exit(1)
 	}
+
 	defer func() {
 		if err := dir.Close(); err != nil {
 			fmt.Println("Ошибка при закрытии директории:", err)
@@ -25,9 +27,56 @@ func Finder(directoryPath string, flagF, flagD, flagSL bool) {
 
 	for _, file := range files {
 		fullPath := filepath.Join(directoryPath, file.Name())
-		if file.IsDir() {
+
+		fileInfo, err := os.Lstat(fullPath)
+		if err != nil {
+			fmt.Println("Ошибка получения информации о файле:", err)
+			continue
+		}
+
+		if fileInfo.Mode()&os.ModeSymlink != 0 {
+			if flagSL {
+				// Чтение цели символической ссылки
+				linkTarget, err := os.Readlink(fullPath)
+				if err != nil {
+					fmt.Printf("%s -> [broken]\n", fullPath)
+				} else {
+					// Проверка, существует ли целевой объект ссылки
+					if _, err := os.Stat(fullPath); os.IsNotExist(err) {
+						fmt.Printf("%s -> [broken]\n", fullPath)
+					} else {
+						fmt.Printf("%s -> %s\n", fullPath, linkTarget)
+					}
+				}
+			}
+			continue
+		}
+
+		if flagF && !fileInfo.IsDir() {
+			fmt.Println(fullPath)
+		}
+
+		if flagD && fileInfo.IsDir() {
+			fmt.Println(fullPath)
+		}
+
+		if fileInfo.IsDir() {
 			Finder(fullPath, flagF, flagD, flagSL)
 		}
-		fmt.Println(fullPath)
+
 	}
+
+}
+
+func ParseFlag() (bool, bool, bool) {
+	flagFile := flag.Bool("f", false, "Флаг для отображения файлов")
+	flagDir := flag.Bool("d", false, "Флаг для отображения директорий")
+	flagLink := flag.Bool("sl", false, "Флаг для отображения символических ссылок")
+	flag.Parse()
+	if !*flagFile && !*flagDir && !*flagLink {
+		*flagFile = true
+		*flagDir = true
+		*flagLink = true
+	}
+	return *flagFile, *flagDir, *flagLink
 }
