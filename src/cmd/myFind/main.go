5e86726ee3f9fa2be808b3e69c787f0fae17c792
jenisepl <jenisepl@student.21-school.ru>
2024-09-21 13:40:14 +0300

    add main for 1 task, makefile

--- src/cmd/myFind/main.go
+++ src/cmd/myFind/main.go
@@ -1,1 +1,9 @@
 package main
+
+import (
+	"github.com/aventhis/go-bootcamp-file-utils/src/internal/app"
+)
+
+func main() {
+	app.MyFind()
+}
---
+++ src/internal/app/app.go
@@ -0,0 +1,7 @@
+package app
+
+import "fmt"
+
+func MyFind() {
+	fmt.Println("Test")
+}
