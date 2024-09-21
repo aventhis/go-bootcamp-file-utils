2416bfd773d356022c823a2421db0e9c73215f90
jenisepl <jenisepl@student.21-school.ru>
2024-09-21 17:05:12 +0300

    add correct files

--- src/internal/service/finder.go
+++ src/internal/service/finder.go
@@ -7,6 +7,21 @@ import (
 	"path/filepath"
 )
 
+// ParseFlag - парсинг флагов командной строки
+func ParseFlag() (bool, bool, bool) {
+	flagFile := flag.Bool("f", false, "Флаг для отображения файлов")
+	flagDir := flag.Bool("d", false, "Флаг для отображения директорий")
+	flagLink := flag.Bool("sl", false, "Флаг для отображения символических ссылок")
+	flag.Parse()
+	if !*flagFile && !*flagDir && !*flagLink {
+		*flagFile = true
+		*flagDir = true
+		*flagLink = true
+	}
+	return *flagFile, *flagDir, *flagLink
+}
+
+// readDir - открытие и чтение директории
 func readDir(directoryPath string) []os.FileInfo {
 	dir, err := os.Open(directoryPath)
 	if err != nil {
@@ -28,61 +43,51 @@ func readDir(directoryPath string) []os.FileInfo {
 	return files
 }
 
+// processSymlink - Функция для обработки символических ссылок
+func processSymlink(fullPath string) {
+	linkTarget, err := os.Readlink(fullPath)
+	if err != nil {
+		fmt.Printf("%s -> [broken]\n", fullPath)
+	} else {
+		// Проверка, существует ли целевой объект ссылки
+		if _, err := os.Stat(fullPath); os.IsNotExist(err) {
+			fmt.Printf("%s -> [broken]\n", fullPath)
+		} else {
+			fmt.Printf("%s -> %s\n", fullPath, linkTarget)
+		}
+	}
+}
+
 func Finder(directoryPath string, flagF, flagD, flagSL bool) {
 	files := readDir(directoryPath)
 
 	for _, file := range files {
 		fullPath := filepath.Join(directoryPath, file.Name())
 
+		// Получение информации о файле или символической ссылке
 		fileInfo, err := os.Lstat(fullPath)
 		if err != nil {
 			fmt.Println("Ошибка получения информации о файле:", err)
 			continue
 		}
 
+		// Обработка символической ссылки
 		if fileInfo.Mode()&os.ModeSymlink != 0 {
 			if flagSL {
-				// Чтение цели символической ссылки
-				linkTarget, err := os.Readlink(fullPath)
-				if err != nil {
-					fmt.Printf("%s -> [broken]\n", fullPath)
-				} else {
-					// Проверка, существует ли целевой объект ссылки
-					if _, err := os.Stat(fullPath); os.IsNotExist(err) {
-						fmt.Printf("%s -> [broken]\n", fullPath)
-					} else {
-						fmt.Printf("%s -> %s\n", fullPath, linkTarget)
-					}
-				}
+				processSymlink(fullPath)
 			}
 			continue
 		}
 
-		if flagF && !fileInfo.IsDir() {
-			fmt.Println(fullPath)
-		}
-
-		if flagD && fileInfo.IsDir() {
-			fmt.Println(fullPath)
-		}
-
+		// Обработка файлов и директорий
 		if fileInfo.IsDir() {
+			if flagD {
+				fmt.Println(fullPath)
+			}
 			Finder(fullPath, flagF, flagD, flagSL)
+		} else if flagF {
+			fmt.Println(fullPath)
 		}
-
 	}
 
-}
-
-func ParseFlag() (bool, bool, bool) {
-	flagFile := flag.Bool("f", false, "Флаг для отображения файлов")
-	flagDir := flag.Bool("d", false, "Флаг для отображения директорий")
-	flagLink := flag.Bool("sl", false, "Флаг для отображения символических ссылок")
-	flag.Parse()
-	if !*flagFile && !*flagDir && !*flagLink {
-		*flagFile = true
-		*flagDir = true
-		*flagLink = true
-	}
-	return *flagFile, *flagDir, *flagLink
 }
