0b49cc4ea7a4063aed27d5f74734587a4b92b2ec
jenisepl <jenisepl@student.21-school.ru>
2024-09-26 18:41:50 +0300

    add countWords, countStr, countChar

--- src/cmd/myWc/main.go
+++ src/cmd/myWc/main.go
@@ -1,5 +1,103 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"unicode/utf8"
+)
 
+func main() {
+	flagW := flag.Bool("w", false, "Флаг для подсчета слов")
+	flagL := flag.Bool("l", false, "Флаг для подсчета строк")
+	flagM := flag.Bool("m", false, "Флаг для подсчета символов")
+
+	flag.Parse()
+
+	if *flagW && *flagL || *flagW && *flagM || *flagL && *flagM {
+		fmt.Println("Ошибка: может бытьь указан только один флаг")
+		fmt.Println("Usage: ./myWc -m input4.txt")
+		os.Exit(1)
+	}
+
+	files := flag.Args()
+
+	if len(files) == 0 {
+		fmt.Println("Usage: ./myWc -m input4.txt input5.txt input6.txt")
+		os.Exit(1)
+	}
+
+	fmt.Println(files)
+
+	//переопределяем функцию
+	var countFunc func(file *os.File) (int, error)
+	if *flagW {
+		countFunc = countWords
+	} else if *flagL {
+		countFunc = countStr
+	} else if *flagM {
+		countFunc = countChar
+	}
+
+	for _, file := range files {
+		processFile(file, countFunc)
+	}
+
+}
+
+func processFile(filePath string, countFunc func(*os.File) (int, error)) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("Ошибка открытия файла", err)
+		return
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			fmt.Println("Ошибка закрытия файла", err)
+			return
+		}
+	}()
+
+	count, err := countFunc(file)
+	if err != nil {
+		fmt.Println("Ошибка обработки файла", filePath, err)
+		return
+	}
+
+	fmt.Printf("%d\t%s\n", count, filePath)
+
+}
+
+func countWords(file *os.File) (int, error) {
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return -1, errors.New("ошибка чтения файла")
+	}
+
+	words := strings.Fields(string(data))
+	return len(words), nil
+}
+
+func countStr(file *os.File) (int, error) {
+	count := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		count++
+	}
+	if err := scanner.Err(); err != nil {
+		return -1, err
+	}
+	return count, nil
+}
+
+func countChar(file *os.File) (int, error) {
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return -1, errors.New("ошибка чтения файла")
+	}
+	return utf8.RuneCountInString(string(data)), nil
 }
