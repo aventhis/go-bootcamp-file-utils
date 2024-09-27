9d68c5a729d34532ff23237ff844e07a83a2ded4
jenisepl <jenisepl@student.21-school.ru>
2024-09-27 17:47:02 +0300

    add parse flag for rotate

--- src/cmd/myRotate/main.go
+++ src/cmd/myRotate/main.go
@@ -1,9 +1,19 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	flagA := flag.Bool("a", false, "flag indicating the directory for archives")
+	flagA := flag.String("a", "", "flag indicating the directory for archives")
 	flag.Parse()
 
+	if len(flag.Args()) != 1 {
+		fmt.Println("Usage: myRotate [-a <archive_directory>] <log_file1> [<log_file2> ...]")
+		os.Exit(1)
+	}
+
+	fmt.Println(*flagA)
 }
